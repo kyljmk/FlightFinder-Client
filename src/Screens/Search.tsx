79d3772963e4d5da804528aa61b61278c3b46fb5
kyljmk <k.mansonkullin@googlemail.com>
2022-12-13 00:02:00 +0100

    Add Radio Button Functionality

--- src/Screens/Search.tsx
+++ src/Screens/Search.tsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import React, { FormEvent, SyntheticEvent, useState } from "react";
 import { DateRangePicker } from "react-date-range";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 
 function Search() {
     const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [flightOption, setFlightOption] = useState<string>("");
   
     const selectionRange = {
         startDate: startDate,
@@ -15,12 +16,27 @@ function Search() {
 
       const handleSelect = (ranges: any) => {
         setStartDate(ranges.selection.startDate);
-        setEndDate(ranges.selection.endDate);
+        if (flightOption == "One-Way") {
+            setEndDate(ranges.selection.startDate);
+        } else {
+            setEndDate(ranges.selection.endDate);
+        }
       };
+
+      const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFlightOption(e.target.value)
+      }
+
+      console.log(startDate);
+      console.log(endDate);
 
     return (
         <div>
             <h1>Search</h1>
+            <div onChange={handleChange}>
+                <input type="radio" value="One-Way" name="flightOption" />
+                <input type="radio" value="Return" name="flightOption" />
+            </div>
             <DateRangePicker
             ranges={[selectionRange]}
             minDate={new Date()}
